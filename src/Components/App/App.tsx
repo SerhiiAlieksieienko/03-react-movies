b245870543d6e30b6f2845dcd61532ec5dda4b9f
Serhii Alieksieienko <in3ruder@hotmail.com>
2025-05-24 18:55:51 +0300

    try-2

--- src/Components/App/App.tsx
+++ src/Components/App/App.tsx
@@ -33,7 +33,7 @@ export default function App() {
         toast.error("No movies found for your request.");
       }
       setMovies(fetchedMovies);
-    } catch (err) {
+    } catch  {
       setError(true);
     } finally {
       setLoading(false);
